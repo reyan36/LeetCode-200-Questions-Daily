2879af3583badd9f7484a34e995e8b49cb103928
Reyan Arshad <reyanarshad36@gmail.com>
2025-11-26 17:38:36 +0500

    Added 680 Valid Palindrome II Solution

---
+++ 680-Valid-Palindrome-II/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        n = len(s)
+        l = 0
+        r = n-1
+
+        while l < r :
+            if s[l] != s[r]:
+                skipL = s[l+1 : r+1] 
+                skipR = s[l : r] 
+                return (skipL==skipL[::-1] or skipR==skipR[::-1])
+            
+            l = l+1
+            r = r-1
+
+        return True
